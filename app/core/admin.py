6440e7cefcfe1eea5d26882b40bfc4bb66f34d97
TOMCAT26\skyju <professional.it919@gmail.com>
2024-09-02 22:23:34 +0800

    add create user

--- app/core/admin.py
+++ app/core/admin.py
@@ -24,9 +24,24 @@ class UserAdmin(BaseUserAdmin):
                     'is_superuser',
                 )
             },
-            (_('Important dates'), {'fields': ('last_login',)}),
-        )
-        readonly_fields = ['last_login']
+            
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldset = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
     )
 
 
